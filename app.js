d40d3a65755bbaab3518d072ec2ca8b79c4d7fee
Manohar R <142599616+Manoharr108@users.noreply.github.com>
2024-02-08 19:18:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 setInterval(function(){
     let a = new Date()
-    let h = a.getHours() 
+    let h = a.getHours() -12
     let m = a.getMinutes()
     let s = a.getSeconds()
     let date = a.getDate()
     let day = a.getDay()
     let year = a.getFullYear()
+    console.log(h)
     if(h<10){
         document.getElementById("hr").innerHTML = "0"+h;
     }
@@ -60,4 +61,17 @@ setInterval(function(){
             break;
     }
     document.getElementById("yr").innerHTML = year
+    let colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+    '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+    '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+    '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+    '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+    '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+    '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+    '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+    '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+    '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+    sec.style.color = colors[Math.floor(Math.random()* colors.length)]
+    document.getElementsByClassName("box")[0].setAttribute("id","box_id")
+    box_id.style.border = "8px solid" +colors[Math.floor(Math.random()* colors.length)]
 },1000)
